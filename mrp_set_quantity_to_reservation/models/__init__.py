d4c567bac8220d042282293e196b450b811c9438
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-04-21 18:01:18 +0100

    TA#76177 [ADD] : mrp_set_quantity_to_reservation (#25)
    * TA#76177 [ADD] : mrp_set_quantity_to_reservation
    
    * TA#76177 [ADD] : mrp_set_quantity_to_reservation
    
    * TA#76177 [ADD] : mrp_set_quantity_to_reservation
    
    * TA#76177 [ADD] : mrp_set_quantity_to_reservation
    
    * TA#76177 [ADD] : mrp_set_quantity_to_reservation

---
+++ mrp_set_quantity_to_reservation/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Tecnativa
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl
+
+from . import mrp_production
+from . import stock_move
